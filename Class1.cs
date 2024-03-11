16ac2de71bd4843ccb31599a005af654e1161571
ArtValg <152530322+ArtValg@users.noreply.github.com>
2024-03-11 21:58:16 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThreadApp
+{
+    public class Execute
+    {
+        public static void Exec()
+        {
+            int i = 0;
+            while(i != 1)
+            {
+                Console.WriteLine("Поток2 " + DateTime.Now.ToString("h:mm:ss tt"));
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread firstThread = new Thread(Execute.Exec);
+            firstThread.Start();
+            int i = 0;
+
+            while(i != 1)
+            {
+                Console.WriteLine("Поток1 " + DateTime.Now.ToString("h:mm:ss tt"));
+                Thread.Sleep(150);
+            }
+            
+        }
+    }
+}
